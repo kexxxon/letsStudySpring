8163b4d1dd3e7aba814a2bc6d57cc6610dd6009d
kexxxon <dev.kexon@gmail.com>
2022-08-28 22:11:55 +0900

    first commit

---
+++ section3-practical/src/main/java/com/cStates/Section3PracticalApplication.java
@@ -0,0 +1,13 @@
+package com.cStates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3PracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3PracticalApplication.class, args);
+	}
+
+}
---
+++ section3-practical/src/test/java/com/cStates/Section3PracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cStates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section3PracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
