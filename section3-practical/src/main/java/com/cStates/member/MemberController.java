c1dc805189dd116a929b9ea3159fcbc061835351
kexxxon <dev.kexon@gmail.com>
2022-08-29 02:03:23 +0900

    refactor: MemberController with DTO

--- section3-practical/src/main/java/com/cStates/member/MemberController.java
+++ section3-practical/src/main/java/com/cStates/member/MemberController.java
@@ -1,47 +1,64 @@
 package com.cStates.member;
 
+import com.cStates.dto.MemberPatchDto;
+import com.cStates.dto.MemberPostDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/v1/members") // (1) produces 설정 제거
+@RequestMapping("/v1/members")
 public class MemberController {
+    // 회원 정보 등록
     @PostMapping
-    public ResponseEntity postMember(@RequestParam("email") String email,
-                                     @RequestParam("name") String name,
-                                     @RequestParam("phone") String phone) {
+    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
 
-        // (2) request json -> Map object
-        Map<String, String> map = new HashMap<>();
-        map.put("email", email);
-        map.put("name", name);
-        map.put("phone", phone);
-
-        // (3) 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{member-id}")
-    public ResponseEntity getMember(@PathVariable("member-id")long memberId) {
+    // 한 명의 회원 정보 조회
+    @GetMapping("/{memberId}")
+    public ResponseEntity getMember(@PathVariable("memberId")long memberId) {
         System.out.println("# memberId: " + memberId);
 
         // not implementation
 
-        // (4) 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<Map>(HttpStatus.OK);
     }
 
+    // 모든 회원 정보 조회
     @GetMapping
     public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
         // not implementation
 
-        // (5) 리턴 값을 ResponseEntity 객체로 변경
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 회원 정보 수정
+    @PatchMapping("/{memberId}")
+    public ResponseEntity patchMember(@PathVariable("memberId") long memberId,
+                                      @RequestBody MemberPatchDto memberPatchDto) {
+
+        memberPatchDto.setMemberId(memberId);
+        memberPatchDto.setName("홍길동");
+
+        // No need Business Logic
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMember(@PathVariable("memberId") long memberId) {
+
+        // No nee Business Logic
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
