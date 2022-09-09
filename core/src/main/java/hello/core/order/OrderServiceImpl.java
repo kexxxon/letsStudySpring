cad808d96934be3633d12784ab916476ee9a2199
kexxxon <dev.kexon@gmail.com>
2022-09-09 23:20:31 +0900

    the way of DI - Constructor DI

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,12 +5,19 @@ import hello.core.member.MemberRepository;
 import hello.core.discount.DiscountPolicy;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // Constructor DI - when constructor is only one, then can drop out @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        // DI test
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("discountPolicy = " + discountPolicy);
+
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
